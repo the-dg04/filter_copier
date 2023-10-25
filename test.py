a105b7ed6a99d491d512b78d307e7508a8dd2f10
the-dg04 <devgup04@gmail.com>
2023-10-25 12:53:12 +0530

    added moved images to public

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import cv2
 import numpy as np
 from src.filter.filter import Filter
+import os
 
-raw_img=cv2.imread('raw_training_image.jpg')
-filtered_img=cv2.imread('filtered_training_image.jpg')
+image_directory=os.path.join(os.getcwd(),'public')
+
+raw_img=cv2.imread(os.path.join(image_directory,'raw_training_image.jpg'))
+filtered_img=cv2.imread(os.path.join(image_directory,'filtered_training_image.jpg'))
 myFilter=Filter()
 myFilter.train(raw_img,filtered_img)
 
-test_img=cv2.imread('test_image.jpg') # your test image goes here
+test_img=cv2.imread(os.path.join(image_directory,'test_image.jpg')) # your test image goes here
 processed_img=myFilter.apply_filter(test_img)
 
 cv2.imshow("Raw Image",test_img)
